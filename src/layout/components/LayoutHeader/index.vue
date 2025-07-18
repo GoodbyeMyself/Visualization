c4fb343c957869fbbbed31017f32d536c58af7de
GoodbyeMyself <15500409521@163.com>
2025-07-18 17:31:30 +0800

    修改  头部按钮区域 显示条件

--- src/layout/components/LayoutHeader/index.vue
+++ src/layout/components/LayoutHeader/index.vue
@@ -1,25 +1,27 @@
 <template>
-  <n-layout-header bordered class="go-header">
-    <header class="go-header-box" :class="{ 'is-project': isProject }">
-      <div class="header-item left">
-        <n-space>
-          <slot name="left"></slot>
-        </n-space>
-      </div>
-      <div class="header-item center">
-        <slot name="center"></slot>
-      </div>
-      <div class="header-item right">
-        <n-space>
-          <slot name="ri-left"> </slot>
-          <go-lang-select></go-lang-select>
-          <theme-color-select></theme-color-select>
-          <go-theme-select></go-theme-select>
-          <slot name="ri-right"> </slot>
-        </n-space>
-      </div>
-    </header>
-  </n-layout-header>
+    <n-layout-header bordered class="go-header">
+        <header class="go-header-box" :class="{ 'is-project': isProject }">
+            <div class="header-item left">
+                <n-space>
+                    <slot name="left"></slot>
+                </n-space>
+            </div>
+            <div class="header-item center">
+                <slot name="center"></slot>
+            </div>
+            <div class="header-item right">
+                <n-space>
+                    <slot name="ri-left"> </slot>
+                    <template v-if="showHeaderComponents">
+                        <go-lang-select></go-lang-select>
+                        <theme-color-select></theme-color-select>
+                        <go-theme-select></go-theme-select>
+                    </template>
+                    <slot name="ri-right"> </slot>
+                </n-space>
+            </div>
+        </header>
+    </n-layout-header>
 </template>
 
 <script setup lang="ts">
@@ -33,33 +35,39 @@ import { PageEnum } from '@/enums/pageEnum'
 const route = useRoute()
 
 const isProject = computed(() => {
-  return route.fullPath === PageEnum.BASE_HOME_ITEMS
+    return route.fullPath === PageEnum.BASE_HOME_ITEMS
+})
+
+// 判断是否显示头部组件（只在 items 和 my-template 页面显示）
+const showHeaderComponents = computed(() => {
+    return route.fullPath === PageEnum.BASE_HOME_ITEMS || 
+           route.fullPath === PageEnum.BASE_HOME_TEMPLATE
 })
 </script>
 
 <style lang="scss" scoped>
 @include go(header) {
-  &-box {
-    display: flex;
-    justify-content: space-between;
-    &.is-project {
-      grid-template-columns: none;
+    &-box {
+        display: flex;
+        justify-content: space-between;
+        &.is-project {
+            grid-template-columns: none;
+        }
+        .header-item {
+            display: flex;
+            align-items: center;
+            &.left {
+                justify-content: start;
+            }
+            &.center {
+                justify-content: center;
+            }
+            &.right {
+                justify-content: end;
+            }
+        }
+        height: $--header-height;
+        padding: 0 20px 0 60px;
     }
-    .header-item {
-      display: flex;
-      align-items: center;
-      &.left {
-        justify-content: start;
-      }
-      &.center {
-        justify-content: center;
-      }
-      &.right {
-        justify-content: end;
-      }
-    }
-    height: $--header-height;
-    padding: 0 20px 0 60px;
-  }
 }
 </style>
--- src/layout/components/LayoutHeaderPro/index.vue
+++ src/layout/components/LayoutHeaderPro/index.vue
@@ -1,21 +1,34 @@
 <template>
-  <layout-header>
-    <template #left>
-      <slot name="left"></slot>
-    </template>
-    <template #center>
-      <slot name="center"></slot>
-    </template>
-    <template #ri-left>
-      <slot name="ri-left"></slot>
-    </template>
-    <template #ri-right>
-      <go-user-info></go-user-info>
-      <slot name="ri-right"></slot>
-    </template>
-  </layout-header>
+    <layout-header>
+        <template #left>
+            <slot name="left"></slot>
+        </template>
+        <template #center>
+            <slot name="center"></slot>
+        </template>
+        <template #ri-left>
+            <slot name="ri-left"></slot>
+        </template>
+        <template #ri-right>
+            <template v-if="showHeaderComponents">
+                <go-user-info></go-user-info>
+            </template>
+            <slot name="ri-right"></slot>
+        </template>
+    </layout-header>
 </template>
 <script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
 import { LayoutHeader } from '@/layout/components/LayoutHeader'
 import { GoUserInfo } from '@/components/GoUserInfo'
+import { PageEnum } from '@/enums/pageEnum'
+
+const route = useRoute()
+
+// 判断是否显示头部组件（只在 items 和 my-template 页面显示）
+const showHeaderComponents = computed(() => {
+    return route.fullPath === PageEnum.BASE_HOME_ITEMS || 
+           route.fullPath === PageEnum.BASE_HOME_TEMPLATE
+})
 </script>
