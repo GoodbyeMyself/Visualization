84440efe249487507f3174755b8bc12fe5d098c7
GoodbyeMyself <15500409521@163.com>
2025-07-21 18:04:05 +0800

    物料名称调整

--- src/views/project/items/components/ProjectItemsList/hooks/useData.hook.ts
+++ src/views/project/items/components/ProjectItemsList/hooks/useData.hook.ts
@@ -7,31 +7,31 @@ export const useDataListInit = () => {
     const list = ref<ChartList>([
         {
             id: 1,
-            title: '物料1-假数据不可用',
+            title: '物料1',
             release: true,
             label: '官方案例'
         },
         {
             id: 2,
-            title: '物料2-假数据不可用',
+            title: '物料2',
             release: false,
             label: '官方案例'
         },
         {
             id: 3,
-            title: '物料3-假数据不可用',
+            title: '物料3',
             release: false,
             label: '官方案例'
         },
         {
             id: 4,
-            title: '物料4-假数据不可用',
+            title: '物料4',
             release: false,
             label: '官方案例'
         },
         {
             id: 5,
-            title: '物料5-假数据不可用',
+            title: '物料5',
             release: false,
             label: '官方案例'
         }
