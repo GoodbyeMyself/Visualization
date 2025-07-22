a94de81fccdf3446f3d06fbc051f8f44720d3b70
GoodbyeMyself <15500409521@163.com>
2025-07-22 15:16:25 +0800

    文件方法封装

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -3,19 +3,19 @@
  * @param { File } file 文件对象
  */
 export const readFile = (file: File) => {
-  return new Promise((resolve: Function) => {
-    try {
-      const reader = new FileReader()
-      reader.onload = (evt: ProgressEvent<FileReader>) => {
-        if (evt.target) {
-          resolve(evt.target.result)
+    return new Promise((resolve: Function) => {
+        try {
+            const reader = new FileReader()
+            reader.onload = (evt: ProgressEvent<FileReader>) => {
+                if (evt.target) {
+                    resolve(evt.target.result)
+                }
+            }
+            reader.readAsText(file)
+        } catch (error) {
+            window['$message'].error('文件读取失败！')
         }
-      }
-      reader.readAsText(file)
-    } catch (error) {
-      window['$message'].error('文件读取失败！')
-    }
-  })
+    })
 }
 
 /**
@@ -25,16 +25,16 @@ export const readFile = (file: File) => {
  * @param fileSuffix 
  */
 export const downloadByA = (url: string, filename = new Date().getTime(), fileSuffix?: string) => {
-  const ele = document.createElement('a') // 创建下载链接
-  ele.download = `${filename}.${fileSuffix}` //设置下载的名称
-  ele.style.display = 'none' // 隐藏的可下载链接
-  // 字符内容转变成blob地址
-  ele.href = url
-  // 绑定点击时间
-  document.body.appendChild(ele)
-  ele.click()
-  // 然后移除
-  document.body.removeChild(ele)
+    const ele = document.createElement('a') // 创建下载链接
+    ele.download = `${filename}.${fileSuffix}` //设置下载的名称
+    ele.style.display = 'none' // 隐藏的可下载链接
+    // 字符内容转变成blob地址
+    ele.href = url
+    // 绑定点击时间
+    document.body.appendChild(ele)
+    ele.click()
+    // 然后移除
+    document.body.removeChild(ele)
 }
 
 /**
@@ -44,11 +44,29 @@ export const downloadByA = (url: string, filename = new Date().getTime(), fileSu
  * @param { ?string } fileSuffix 文件名称（默认随机字符）
  */
 export const downloadTextFile = (
-  content: string,
-  filename = new Date().getTime(),
-  fileSuffix?: string
+    content: string,
+    filename = new Date().getTime(),
+    fileSuffix?: string
 ) => {
-  // 字符内容转变成blob地址
-  const blob = new Blob([content])
-  downloadByA(URL.createObjectURL(blob), filename, fileSuffix)
+    // 字符内容转变成blob地址
+    const blob = new Blob([content])
+    downloadByA(URL.createObjectURL(blob), filename, fileSuffix)
+}
+
+/**
+ * * base64转file
+ * @param dataurl 
+ * @param fileName 
+ * @returns 
+ */
+export const base64toFile = (dataurl: string, fileName: string) => {
+    let dataArr = dataurl.split(","),
+        mime = (dataArr as any[])[0].match(/:(.*?);/)[1],
+        bstr = atob(dataArr[1]),
+        n = bstr.length,
+        u8arr = new Uint8Array(n);
+    while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8arr], fileName, { type: mime });
 }
